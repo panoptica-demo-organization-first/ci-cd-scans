072e2b49247f012e71e8de97ba175651636c0468
Haim Raitsev <haimrait@cisco.com>
2024-07-03 11:59:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 
 app.get('/', (req, res) => {
     res.send('Hello World! Test');
